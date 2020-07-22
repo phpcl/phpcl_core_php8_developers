b6839566faadf671c5145dfaeaa1119fa7b7c3e6
dbierer <doug@unlikelysource.com>
2020-07-22 12:16:04 +0700

    2020-07-22 DB: updated refresh + set Dockerfile to switch to master branch

--- refresh.php
+++ refresh.php
@@ -2,6 +2,6 @@
 define('REPO', '/srv/repo');
 chdir(REPO);
 $message = 'Repo refresh status: <br>'
-		 . '<pre>' . shell_exec('git stash') . '</pre>' . PHP_EOL;
+		 . '<pre>' . shell_exec('git stash') . '</pre>' . PHP_EOL
 		 . '<pre>' . shell_exec('git pull') . '</pre>' . PHP_EOL;
 include __DIR__ . '/index.php';
