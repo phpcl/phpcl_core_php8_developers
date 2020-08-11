ac0a4bc2a50dc8c48e1074c72e23af537ad1cd31
dbierer <doug@unlikelysource.com>
2020-08-11 12:47:54 +0700

    2020-08-11 DB: developing lab env for PHP 8 course

--- index.php
+++ index.php
@@ -4,27 +4,16 @@ if (!defined('EXAMPLES')) define('EXAMPLES', 'examples');
 define('COLS', 4);
 // init vars
 $flag    = TRUE;
-$format  = '<td><a href="run.php?file=%s">%s</a></td>';
+$format  = '<a href="run.php?file=%s">%s</a>';
 $path    = str_replace('//', '/', __DIR__ . '/' . EXAMPLES);
 $list    = new ArrayIterator(glob($path . '/*.php'));
 // output file list
 if (empty($output)) $output = '';
 if (empty($message)) $message = '';
-$examples  = '<table width="100%">' . PHP_EOL;
-while ($flag) {
-    $examples .= '<tr>' . PHP_EOL;
-    for ($x = 0; $x < COLS; $x++) {
-		if ($list->valid()) {
-			$name = basename($list->current());
-			$list->next();
-			$examples .= sprintf($format, $name, $name);
-		} else {
-			$flag = FALSE;
-			$examples .= '<td>&nbsp;</td>';
-		}
-		$examples .= PHP_EOL;
-	}
-	$examples .= '</tr>' . PHP_EOL;
+$examples = '';
+while ($list->valid()) {
+	$name = basename($list->current());
+	$list->next();
+	$examples .= str_pad(sprintf($format, $name, $name), 20);
 }
-$examples .= '</table>' . PHP_EOL;
 include __DIR__ . '/home.phtml';
