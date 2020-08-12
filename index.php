5deccc74c2863e8c4427aecb446bc1bc970a2bd5
dbierer <doug@unlikelysource.com>
2020-08-12 10:28:01 +0700

    2020-08-12 DB: updated examples home page

--- index.php
+++ index.php
@@ -4,16 +4,10 @@ if (!defined('EXAMPLES')) define('EXAMPLES', 'examples');
 define('COLS', 4);
 define('FORMAT', '<div class="col-md-3"><a href="run.php?file=%s">%s</a></div>' . PHP_EOL);
 // init vars
-$flag     = TRUE;
-$path     = str_replace('//', '/', __DIR__ . '/' . EXAMPLES);
-$examples = new ArrayIterator(glob($path . '/*.php'));
+$flag = TRUE;
+$path = str_replace('//', '/', __DIR__ . '/' . EXAMPLES);
+$list = new ArrayIterator(glob($path . '/*.php'));
 // output file list
 if (empty($output)) $output = '';
 if (empty($message)) $message = '';
-$examples = '';
-while ($list->valid()) {
-	$name = basename($list->current());
-	$list->next();
-	$examples .= str_pad(sprintf($format, $name, $name), 20);
-}
 include __DIR__ . '/home.phtml';
