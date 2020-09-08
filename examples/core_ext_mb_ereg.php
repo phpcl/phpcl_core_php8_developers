1832070f093d81d611c9e233d91d8baddd80d5fe
dbierer <doug@unlikelysource.com>
2020-09-08 15:49:53 +0700

    2020-09-08 DB: added mbstring examples + updated Dockerfile

---
+++ examples/core_ext_mb_ereg.php
@@ -0,0 +1,10 @@
+<?php
+// core_ext_mb_ereg.php
+
+$str  = 'ฉันเห็นผู้หญิงไทยสวย 2 คน';
+mb_regex_encoding('UTF-8');
+$mod1 = mb_ereg_replace('2', 51, $str);
+$mod2 = preg_replace('/2/u', 3, $str);
+echo "Original: $str\n";
+echo "Modified Using mb_ereg_replace: $mod1\n";
+echo "Modified Using preg_replace   : $mod2\n";
