639b1a373bdeb382a9a3d93a1558c6f8afff788a
dbierer <doug@unlikelysource.com>
2020-09-10 11:20:46 +0700

    2020-09-10 DB: updated presentation of code examples

--- index.php
+++ index.php
@@ -6,12 +6,30 @@ define('SPACER', '<td>&nbsp;&nbsp;&nbsp;</td>');
 define('FORMAT', '<td style="margin-right: 20px;"><a href="run.php?file=%s">%s</a></td>' . PHP_EOL);
 // init vars
 $flag = TRUE;
-$list = [];
+$iter = NULL;
+$filt = NULL;
 $path = str_replace('//', '/', __DIR__ . '/' . EXAMPLES);
+$categories = [
+	'cool' => 'Cool Stuff',
+	'oop'  => 'OOP PHP',
+	'proc' => 'Procedural PHP',
+	'ext'  => 'PHP Extensions',
+	'error' => 'Error Handling'
+];
 // output file list
 if (empty($message)) $message = '';
 if (empty($output)) {
 	$output = '';
-	$list = new ArrayIterator(glob($path . '/*.php'));
+	$iter   = new ArrayIterator(glob($path . '/*.php'));
+	$iter->asort();
+	// create filter by category
+	$filt = new class ($iter) extends FilterIterator {
+		public $cat = 'cool';
+		public function accept() : bool
+		{
+			$fn = parent::current();
+			return (strpos($fn, $this->cat));
+		}
+	};
 }
 include __DIR__ . '/home.phtml';
