fffeac13fba0c1451baf9ecc42c671aa4bd511de
dbierer <doug@unlikelysource.com>
2020-10-06 12:27:09 +0700

    2020-10-06 DB: Revised str_starts_with example

--- examples/core_cool_starts_with.php
+++ examples/core_cool_starts_with.php
@@ -1,25 +1,35 @@
 <?php
 // core_cool_starts_with.php
-$search = '//';
-$string = '//path/to/some/directory';
+$url = 'http://phptraining.net/logout';
+$check_url_old = function ($url, $start, $end)
+{
+	$msg = '';
+	if (substr($url, 0, strlen($start)) !== $start) {
+		$msg .= "URL does not start with $start\n";
+	}
+	if (strpos(strrev($url), strrev($end)) !== 0) {
+		$msg .= "URL does not end with $end\n";
+	}
+	return $msg;
+};
+echo "Works in PHP 7 and below\n";
+$msg = $check_url_old($url, 'https', 'login');
+echo ($msg) ? $msg : "Proceeding with login\n";
 
-// traditional approach:
-$result = (substr($string, 0, strlen($search)) === $search);
-echo ($result)
-     ? 'Starts with ' . $search
-     : 'Does not start with ' . $search;
-echo "\n";
+$check_url_new = function ($url, $start, $end)
+{
+	$msg = '';
+	if (str_starts_with($url, $start)) {
+		$msg .= "URL does not start with $start\n";
+	}
+	if (str_ends_with($url, $end)) {
+		$msg .= "URL does not end with $end\n";
+	}
+	return $msg;
+};
+if (function_exists('str_starts_with')) {
+	echo "Works in PHP 8\n";
+	$msg = $check_url_old($url, 'https', 'login');
+	echo ($msg) ? $msg : "Proceeding with login\n";
+}
 
-// PHP 8:
-$result = (str_starts_with($string, $search));
-echo ($result)
-     ? 'Starts with ' . $search
-     : 'Does not start with ' . $search;
-echo "\n";
-
-// searching at the end:
-$result = (str_ends_with($string, $search));
-echo ($result)
-     ? 'Ends with ' . $search
-     : 'Does not end with ' . $search;
-echo "\n";
