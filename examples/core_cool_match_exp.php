06af0a8483d6abf5830042eae66df79046e8a68e
dbierer <doug@unlikelysource.com>
2020-10-06 13:58:01 +0700

    2020-10-06 DB: Added more clear Please ask your administrator. expression example'

---
+++ examples/core_cool_match_exp.php
@@ -0,0 +1,43 @@
+<?php
+// core_cool_match_exp.php
+			
+function math_old($a, $b, $op)
+{
+	switch ($op) {
+		case '+' :
+			$result = $a + $b;
+			break;
+		case '-' :
+			$result = $a - $b;
+			break;
+		case '*' :
+			$result = $a * $b;
+			break;
+		case '/' :
+			$result = $a / $b;
+			break;
+		default :
+			$result = 0;
+	}
+	return $result;
+}
+echo "Using switch\n";
+echo math_old(2, 2, '+');
+		
+// exactly the same thing using `match` expression:
+
+echo "\nUsing match expression\n";
+
+function math_new($a, $b, $op)
+{
+	return match ($op) {
+		'+' => $a + $b,
+		'-' => $a - $b,
+		'*' => $a * $b,
+		'/' => $a / $b,
+		'default' => 0
+	}
+}
+echo math_new(2, 2, '+');
+echo "\n";
+
--- examples/core_cool_starts_with.php
+++ examples/core_cool_starts_with.php
@@ -4,12 +4,10 @@ $url = 'http://phptraining.net/logout';
 $check_url_old = function ($url, $start, $end)
 {
 	$msg = '';
-	if (substr($url, 0, strlen($start)) !== $start) {
+	if (substr($url, 0, strlen($start)) !== $start)
 		$msg .= "URL does not start with $start\n";
-	}
-	if (strpos(strrev($url), strrev($end)) !== 0) {
+	if (strpos(strrev($url), strrev($end)) !== 0)
 		$msg .= "URL does not end with $end\n";
-	}
 	return $msg;
 };
 echo "Works in PHP 7 and below\n";
@@ -19,12 +17,10 @@ echo ($msg) ? $msg : "Proceeding with login\n";
 $check_url_new = function ($url, $start, $end)
 {
 	$msg = '';
-	if (str_starts_with($url, $start)) {
+	if (str_starts_with($url, $start))
 		$msg .= "URL does not start with $start\n";
-	}
-	if (str_ends_with($url, $end)) {
+	if (str_ends_with($url, $end))
 		$msg .= "URL does not end with $end\n";
-	}
 	return $msg;
 };
 if (function_exists('str_starts_with')) {
